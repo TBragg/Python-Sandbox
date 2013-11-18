44802487f86b0c82699c56af58326f04ae80c6f8
Taylor Bragg <taylor.bragg@gmail.com>
2013-11-17 22:59:50 -0500

    making changes to elevator.py

--- elevator/elevator.py
+++ elevator/elevator.py
@@ -2,18 +2,22 @@ import os
 import time
 
 # Function definitions
-def displayFloors(person,floors):
+def ElevatorSimulator(e,f):
 	os.system('clear')
-	for x in range(0,floors):
-		if x==person:
-			print '*-----0-----*'
+	print 'Taylor\'s Elevator Simulation Script(TM)'
+	print ''
+	for x in range(0,f):
+		if x==e:
+			print '*----[0]----*'
 		else:
 			print '*-----------*'
 
 os.system('clear')
-
 print 'Starting the Elevator Simulation Script(TM)'
+time.sleep(2)
 
-time.sleep(5)
-
-displayFloors(20)
+floors = 20
+for elevator in range(0,floors):
+	ElevatorSimulator(elevator,floors)
+	time.sleep(0.5)
+print 'Completed!'
