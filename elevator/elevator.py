b939fe2befe97205b82bb649ca6fa825fc4dad71
Taylor Bragg <taylor.bragg@gmail.com>
2013-11-17 22:36:06 -0500

    beginning to program elevator.py

--- elevator/elevator.py
+++ elevator/elevator.py
@@ -1,5 +1,19 @@
 import os
+import time
+
+# Function definitions
+def displayFloors(person,floors):
+	os.system('clear')
+	for x in range(0,floors):
+		if x==person:
+			print '*-----0-----*'
+		else:
+			print '*-----------*'
 
 os.system('clear')
 
 print 'Starting the Elevator Simulation Script(TM)'
+
+time.sleep(5)
+
+displayFloors(20)
