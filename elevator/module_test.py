a8b47d6ab2889c973ae24bae620afcc3f6092959
Taylor Bragg <taylor.bragg@gmail.com>
2013-11-23 17:07:32 -0500

    added ichk import and command call

--- elevator/module_test.py
+++ elevator/module_test.py
@@ -5,6 +5,14 @@ __author__	= "Taylor Bragg"
 
 import sys
 import mymodule
+import ichk
 
 mymodule.sayhi()
 print 'Version',mymodule.version
+
+#input = raw_input('Input: ')
+
+while True:
+	input = raw_input('Input: ')
+	if ichk.isInt(input):
+		break
