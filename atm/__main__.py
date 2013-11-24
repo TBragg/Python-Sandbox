2104b80c9491ac04846dfb60a842a9ac305dd017
Tbragg <taylor.bragg@gmail.com>
2013-11-23 23:31:24 -0500

    commits

---
+++ atm/__main__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+import atm
+
+atm.showBalance()
--- atm/atm.py
+++ atm/atm.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 # Filename: atm.py
 
+import ichk
+
 ATM_BALANCE = 1234.56
+PIN_NUMBER = 1234
+
+def login():
+	
 
 def showBalance():
-	print ATM_BALANCE
+	print "Your balance is: ", ATM_BALANCE
+
+print "Welcome to Taylor's ATM"
 
 # End of atm.py
