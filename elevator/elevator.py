be8e7dad4cfcbd10807d5da4c5254b908fad6c37
Tbragg <taylor.bragg@gmail.com>
2013-11-23 21:19:34 -0500

    flushing out elevator module

--- elevator/elevator.py
+++ elevator/elevator.py
@@ -1,42 +1,13 @@
 #!/usr/bin/python
-
-import os
-import time
-import ichk
+# Filename: elevator.py
 
 BUILDING_HEIGHT = 20
 
-# Function definitions
-def ElevatorSimulator(e,f):
-	os.system('clear')
-	print 'Taylor\'s Elevator Simulation Script(TM)'
-	print ''
-	for x in range(0,f):
-		if x==e:
-			print '*----[0]----*'
-		else:
-			print '*-----------*'
-
-def DisplayBuilding(floor):
+def showBuilding(floor):
 	for x in range(BUILDING_HEIGHT,0,-1):
 		if x==(floor+1):
 			print '||----[0]----||', x
 		else:
 			print '||-----------||', x 
 
-# Still need to work on confirming integer value
-def IntroScreen():
-	os.system('clear')
-	DisplayBuilding(0)
-	while True:
-		input = raw_input('Please enter a valid floor number: ')
-		if ichk.isInt(input):
-			break
-
-os.system('clear')
-print 'Welcome to Taylor\'s Elevator Simulation Script(TM)'
-time.sleep(2)
-
-floor_selection = IntroScreen()
-
-print 'Completed!'
+# End of elevator.py
