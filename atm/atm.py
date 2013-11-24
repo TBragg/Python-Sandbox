8a07da1ae66e7e05faec950daea3a0fff4926475
Tbragg <taylor.bragg@gmail.com>
2013-11-23 23:52:47 -0500

    commit

--- atm/atm.py
+++ atm/atm.py
@@ -5,12 +5,17 @@ import ichk
 
 ATM_BALANCE = 1234.56
 PIN_NUMBER = 1234
+LOGIN_TRIES = 4
 
 def login():
+	count = 0
 	while True:
+		count+=1
 		input = raw_input('Please enter your PIN: ')
 		if ichk.isInt(input) and int(input)==PIN_NUMBER:
 			break
+		else if count == LOGIN_TRIES:
+			print 'You've exceeded the number of login attempts. Goodbye.'
 		else:
 			print 'Incorrect PIN. Please try again.'
 
