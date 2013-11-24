fbd021eed623cfc0d25a010c6ef154a4f4fce92e
Tbragg <taylor.bragg@gmail.com>
2013-11-23 22:22:26 -0500

    Commiting main.py and elevator module

--- elevator/elevator.py
+++ elevator/elevator.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python
 # Filename: elevator.py
 
+import os
+import time
+import ichk
+
 BUILDING_HEIGHT = 20
 
 def showBuilding(floor):
+	os.system('clear')
 	for x in range(BUILDING_HEIGHT,0,-1):
-		if x==(floor+1):
+		if x==(floor):
 			print '||----[0]----||', x
 		else:
 			print '||-----------||', x 
+
+def getFloorChoice():
+	while True:
+		input = raw_input('Please enter your floor choice: ')
+		if ichk.isInt(input) and int(input)<=BUILDING_HEIGHT:
+			break	
+	floor = int(input)+1
+	return floor
+
+def goToFloor(floor):
+	for x in range(1,floor):
+		showBuilding(x)
+		time.sleep(.5)
 
 # End of elevator.py
--- elevator/main.py
+++ elevator/main.py
@@ -1,42 +1,9 @@
 #!/usr/bin/python
 
-import os
-import time
-import ichk
+import elevator
 
-BUILDING_HEIGHT = 20
+elevator.showBuilding(1)
 
-# Function definitions
-def ElevatorSimulator(e,f):
-	os.system('clear')
-	print 'Taylor\'s Elevator Simulation Script(TM)'
-	print ''
-	for x in range(0,f):
-		if x==e:
-			print '*----[0]----*'
-		else:
-			print '*-----------*'
+floor=elevator.getFloorChoice()
 
-def DisplayBuilding(floor):
-	for x in range(BUILDING_HEIGHT,0,-1):
-		if x==(floor+1):
-			print '||----[0]----||', x
-		else:
-			print '||-----------||', x 
-
-# Still need to work on confirming integer value
-def IntroScreen():
-	os.system('clear')
-	DisplayBuilding(0)
-	while True:
-		input = raw_input('Please enter a valid floor number: ')
-		if ichk.isInt(input):
-			break
-
-os.system('clear')
-print 'Welcome to Taylor\'s Elevator Simulation Script(TM)'
-time.sleep(2)
-
-floor_selection = IntroScreen()
-
-print 'Completed!'
+elevator.goToFloor(floor)
